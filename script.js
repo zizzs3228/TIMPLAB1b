7bbdf2dd84d9dcfe0b9224900aa2b66b15353e33
zizzs3228 <73750173+zizzs3228@users.noreply.github.com>
2022-10-15 21:00:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function hashCode(str) {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function noselect() {
+    return false;
+}
+
+let check = true;
+const password = 1601206599;
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyC' && event.ctrlKey && check) {
+        window.getSelection()?.removeAllRanges();
+    }
+});
+document.addEventListener('mousedown', function(click) {
+    if (check) {
+        switch (click.button) {
+            case 0:
+                window.getSelection()?.removeAllRanges();
+                break;
+            case 2:
+                click.preventDefault();
+                let userinput = prompt('Введите пароль для копирования');
+                let hashpas = hashCode(userinput);
+                if (password == hashpas) {
+                    check = false;
+                };
+                break;
+        }
+    }
+});
+//end of script
+
+
+// not script
+document.addEventListener('keydown', function(event) {
+  console.log(event.code)
+});
+
+
